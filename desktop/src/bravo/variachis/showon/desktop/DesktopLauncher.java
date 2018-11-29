4c3a08c8f6b48b0135c58c1d4cbc3e61be22fc16
Tiago Machado <tfamsoad@gmail.com>
2018-11-29 15:46:18 +0000

    Added title and window size

--- desktop/src/bravo/variachis/showon/desktop/DesktopLauncher.java
+++ desktop/src/bravo/variachis/showon/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import bravo.variachis.showon.ShowOn;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "The Show Must Go On";
+		config.width = 800;
+		config.height = 600;
 		new LwjglApplication(new ShowOn(), config);
 	}
 }
