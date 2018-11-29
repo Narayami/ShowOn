717cff8b8254b64657eba07941ed427e02d520d8
Joao Goncalves <joao_andre91@hotmail.com>
2018-11-29 15:33:23 +0000

    cleaned core class

--- core/src/bravo/variachis/showon/ShowOn.java
+++ core/src/bravo/variachis/showon/ShowOn.java
@@ -8,26 +8,18 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class ShowOn extends Game {
-	SpriteBatch batch;
-	Texture img;
+
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }
