1be6d263702646ce7255b879de80c81864e5e058
Tiago Machado <tfamsoad@gmail.com>
2018-11-29 15:26:11 +0000

    Created project

---
+++ desktop/src/bravo/variachis/showon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package bravo.variachis.showon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import bravo.variachis.showon.ShowOn;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ShowOn(), config);
+	}
+}
