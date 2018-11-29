be084b8597d6b9bd49358eafcce2652f4d4e7613
Tiago Machado <tfamsoad@gmail.com>
2018-11-29 17:14:06 +0000

    Updated collision logic

--- core/src/bravo/variachis/showon/ShowOn.java
+++ core/src/bravo/variachis/showon/ShowOn.java
@@ -7,6 +7,14 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
 
 
@@ -16,36 +24,53 @@ public class ShowOn extends Game {
     private Texture playerImg;
     private Rectangle player;
     private OrthographicCamera camera;
+    private TiledMap map;
+    private TiledMapRenderer mapRenderer;
+
+    private int objectLayerId = 5;
+
 
     @Override
     public void create() {
         batch = new SpriteBatch();
+
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 600);
 
-        playerImg = new Texture(Gdx.files.internal("chartest.png"));
+        map = new TmxMapLoader().load("core/assets/basemap.tmx");
+        mapRenderer = new OrthogonalTiledMapRenderer(map);
+
+        playerImg = new Texture(Gdx.files.internal("smallRect.jpg"));
 
         player = new Rectangle();
         player.x = 0;
         player.y = 0;
-        player.width = 64;
-        player.height = 64;
+        player.width = 15;
+        player.height = 15;
 
 
     }
 
     @Override
     public void render() {
+
+        //bg color
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.setProjectionMatrix(camera.combined);
         camera.update();
 
+        mapRenderer.setView(camera);
+        mapRenderer.render();
+
+
         batch.begin();
         batch.draw(playerImg, player.x, player.y);
         batch.end();
 
+
+        //player movement
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             player.x -= SPEED * Gdx.graphics.getDeltaTime();
         }
@@ -57,6 +82,19 @@ public class ShowOn extends Game {
         }
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
             player.y -= SPEED * Gdx.graphics.getDeltaTime();
+        }
+
+
+        TiledMapTileLayer collisionObjectLayer = (TiledMapTileLayer)map.getLayers().get(1);
+        MapObjects objects = collisionObjectLayer.getObjects();
+
+        // there are several other types, Rectangle is probably the most common one
+        for (RectangleMapObject rectangleObject : objects.getByType(RectangleMapObject.class)) {
+
+            Rectangle rectangle = rectangleObject.getRectangle();
+            if (Intersector.overlaps(rectangle, player)) {
+                // collision happened
+            }
         }
 
     }
