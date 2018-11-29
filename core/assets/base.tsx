1d3625ff85e48861d7d3156bff8ef4d67eb5fc38
Joao Goncalves <joao_andre91@hotmail.com>
2018-11-29 17:24:41 +0000

    created .png

--- core/assets/base.tsx
+++ core/assets/base.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Base" tilewidth="8" tileheight="8" tilecount="1666" columns="34">
+<tileset name="base" tilewidth="8" tileheight="8" tilecount="1666" columns="34">
  <image source="Tilles/Itch release tileset base 01.png" width="272" height="392"/>
 </tileset>
